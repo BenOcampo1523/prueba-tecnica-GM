4ee3c5bc50a4341a6c2724f5ff4b4a8e1aa6e72c
BenOcampo1523 <61302749+BenOcampo1523@users.noreply.github.com>
2024-12-08 00:09:56 -0800

    Successful login now redirects to home page

--- app/controllers/auth-controller.js
+++ app/controllers/auth-controller.js
@@ -1,5 +1,7 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const path = require("path");
+const currentDir = process.cwd();
 
 // Importar utils
 const validateRequiredParams = require('../utils/validateRequiredParams');
@@ -9,13 +11,13 @@ const generateAccessToken = (userId) => {
     return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
 };
 
-const login = async (req, res) => {
+const login = async (req, res, next) => {
     try {
-
+        // Obtener datos del body (username, password)
         const { username, password } = req.body;
-
+        // Validación de strings del body
         const missingParam = validateRequiredParams(req.body, ["username", "password"]);
-
+        
         if (missingParam) return res.status(400).send({ error: missingParam });
 
         const results = await executeQuery(
@@ -25,7 +27,11 @@ const login = async (req, res) => {
 
         if (results.length === 0) return res.status(401).json({ error: 'Invalid credentials.' });
 
-        return res.status(200).send({ message: 'User logged in successfully', token: generateAccessToken(results[0].id) });
+        res.redirect('/home');
+        next();
+        
+        //res.sendFile(path.resolve(__dirname, `${currentDir}/public/home.html`));
+//        return res.status(200).send({ message: 'User logged in successfully', token: generateAccessToken(results[0].id) });
     } catch (error) {
         return res.status(500).send({ error: error.message });
     }
